13f3c59489771373e073b219f699819c4a62d533
Eduardo Sodré <edduardooo2011@gmail.com>
2024-08-04 23:30:38 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from '@/src/components/header';
 import CategoryNav from '@/src/components/categoryNav';
 import ProductList from '@/src/components/productList/index';
@@ -6,6 +7,9 @@ import Pagination from '@/src/components/pagination/index';
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>Capputeeno</title>
+      </Head>
       <Header />
       <div className="container">
         <CategoryNav/>
