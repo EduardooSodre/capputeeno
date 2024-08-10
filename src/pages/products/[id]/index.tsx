92bce3bce2d35811ac89663c6d00b80cb4900d46
Eduardo Sodré <edduardooo2011@gmail.com>
2024-08-10 00:02:31 -0300

    description-finalizado

--- src/pages/products/[id]/index.tsx
+++ src/pages/products/[id]/index.tsx
@@ -12,11 +12,24 @@ const ProductPage = () => {
   }
 
   return (
-    <div className={styles.productPage}>
-      <img src={product.image} alt={product.name} className={styles.productImage} />
-      <h1 className={styles.productName}>{product.name}</h1>
-      <p className={styles.productPrice}>{product.price}</p>
-      <p className={styles.productDescription}>{product.description}</p>
+<div className={styles.productDetail}>
+      <button className={styles.returnButton} onClick={() => router.back()}>
+        <img src="/assets/return.png" alt="Voltar" className={styles.returnIcon} /> <p>Voltar</p> </button>
+      <div className={styles.productContainer}>
+        <img src={product.image} alt={product.name} className={styles.productImage} />
+        <div className={styles.productInfo}>
+          <p className={styles.category}>Caneca</p>
+          <h1>{product.name}</h1>
+          <div className={styles.productPrice}>{product.price}</div>
+          <p className={styles.frete}>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
+          <h2>DESCRIÇÃO</h2>
+          <p className={styles.description}>{product.description}</p>
+
+          <button className={styles.button_buy}>
+          <img className={styles.button_shopping__icon}  src="/assets/button-buy.png" alt="Shopping Cart" />Adicionar ao carrinho
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
